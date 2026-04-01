acd0373c8e9cf3e1a3de4ec492e195c51bcb4534
Anand Ganesh <anand.ganesh@anandganesh-CMQFQ9WQ4D.local>
2026-04-01 09:14:58 -0700

    fix: Position Coming Soon tooltip below Debug button

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -8,9 +8,9 @@ import DebugCredentialsModal from './DebugCredentialsModal';
 const ComingSoonTooltip = ({ children }) => (
   <div className="group relative">
     {children}
-    <div className="absolute bottom-full left-1/2 -translate-x-1/2 mb-2 px-3 py-1.5 bg-gray-900 text-white text-xs rounded-lg whitespace-nowrap opacity-0 invisible group-hover:opacity-100 group-hover:visible transition-all z-50">
+    <div className="absolute top-full left-1/2 -translate-x-1/2 mt-2 px-3 py-1.5 bg-gray-900 text-white text-xs rounded-lg whitespace-nowrap opacity-0 invisible group-hover:opacity-100 group-hover:visible transition-all z-50">
       Coming Soon
-      <div className="absolute top-full left-1/2 -translate-x-1/2 border-4 border-transparent border-t-gray-900"></div>
+      <div className="absolute bottom-full left-1/2 -translate-x-1/2 border-4 border-transparent border-b-gray-900"></div>
     </div>
   </div>
 );
