1de1742c96c1275634ed379855f805ad01e63d88
Anand Ganesh <anand.ganesh@anandganesh-CMQFQ9WQ4D.local>
2026-04-16 03:33:28 -0700

    add request logging

--- server/server.js
+++ server/server.js
@@ -9,6 +9,11 @@ const PORT = process.env.PORT || 5252;
 app.use(cors());
 app.use(express.json());
 
+app.use((req, res, next) => {
+  console.log(`${new Date().toISOString()} - ${req.method} ${req.url}`);
+  next();
+});
+
 const getCredentials = (req) => {
   const isDebugMode = req.headers['x-debug-mode'] === 'true';
   
